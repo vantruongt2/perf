9b8780060b747c4f198d40dcaf4cf55d4385ee10
tien.tran <tienletjanet@gmail.com>
2019-04-04 16:34:59 +0700

    restructure docker file

--- execution/networksample.sh
+++ execution/networksample.sh
@@ -11,24 +11,17 @@ threadCountForLast=$5
 SUB_NET="172.20.0.0/16"
 CLIENT_IP=172.20.0.100
 BASE_SUBNET_IP_1="172.20.0."
-BASE_SUBNET_IP_2="172.20."
 LGSG_IP="192.168.172.24"
 BASE_CHILD_IP=2
-LAST_SERVER=
 
 declare -a SERVER_IPS
-for ((counter=0; counter<($numberOfSlave - 1); counter+=1)); do
-	SERVER_IPS+=("$BASE_SUBNET_IP_1$BASE_CHILD_IP")
-	((BASE_CHILD_IP+=1))
-done
 
 CONTAINER_SERVER_IPS=("${SERVER_IPS[@]}")
 
 if [[ -z "${threadCountForLast}" || "${threadCountForLast}" -eq "0" ]]; then
-	echo "NO need thread count for last"
-else
-	LAST_SERVER=${LGSG_IP}	
-	SERVER_IPS+=("${LGSG_IP}")
+  echo "NO need thread count for last"
+else  
+  SERVER_IPS+=("${LGSG_IP}")
 fi
 
 echo "slave list:" ${SERVER_IPS[@]}
@@ -48,7 +41,7 @@ docker network create --subnet=$SUB_NET $TEST_NET
 echo "Create JMeter container slave"
 for IP_ADD in "${CONTAINER_SERVER_IPS[@]}"
 do
-echo adding $IP_ADD
+echo creating $IP_ADD
 docker run \
 -dit \
 --net $TEST_NET --ip $IP_ADD \
@@ -72,7 +65,7 @@ docker run \
   -Jserver.rmi.ssl.keystore.file=${jmeter_path}/keys/rmi_keystore.jks \
   -Jclient.rmi.localport=7000 \
   -Jremote_hosts $(echo $(printf ",%s" "${SERVER_IPS[@]}") | cut -c 2-) \
-  -JnumberOfThreads=${threadCountBase} -JappName=${targetVersion} -JloopCount=1 \
+  -DnumberOfThreads=${threadCountBase} -DappName=${targetVersion} -DloopCount=1 -DdbHost="http://192.168.190.125:8086" -DdbName=jmeter \
   -t ${jmeter_path}/jmx/Weather.jmx \
   -l ${jmeter_path}/client/result_${timestamp}.jtl \
   -j ${jmeter_path}/client/jmeter_${timestamp}.log 
