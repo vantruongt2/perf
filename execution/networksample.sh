e91de86820c0b785a3cce8467aa74b09ee51b03e
tien.tran <tienletjanet@gmail.com>
2019-04-05 09:29:20 +0700

    support lgsg slave

--- execution/networksample.sh
+++ execution/networksample.sh
@@ -1,41 +1,42 @@
 #!/bin/bash
 
+# example: ./networksample.sh /home/test/Desktop/workspace/perf Weather.jmx 2.0 2 5 6
 data_folder=$1
-targetVersion=$2
-numberOfSlave=$3
-threadCountBase=$4
-threadCountForLast=$5
+jmx_fileName=$2
+targetVersion=$3
+numberOfSlave=$4
+threadCountBase=$5
+threadCountForLast=$6
 
 #1
-SUB_NET="172.18.0.0/16"
-CLIENT_IP=172.18.0.100
-BASE_SUBNET_IP_1="172.18.0."
-BASE_SUBNET_IP_2="172.18."
+SUB_NET="172.20.0.0/16"
+CLIENT_IP=172.20.0.100
+BASE_SUBNET_IP_1="172.20.0."
+LGSG_IP="192.168.172.24"
 BASE_CHILD_IP=2
-LAST_SERVER=
 
 declare -a SERVER_IPS
-for ((counter=0; counter<$numberOfSlave; counter+=1)); do
-	SERVER_IPS+=("$BASE_SUBNET_IP_1$BASE_CHILD_IP")
-	((BASE_CHILD_IP+=1))
+
+for ((counter=0; counter<$numberOfSlave; counter+=1)); do 
+  SERVER_IPS+=("$BASE_SUBNET_IP_1$BASE_CHILD_IP")
+  ((BASE_CHILD_IP+=1))
 done
 
-ORIGINAL_SERVER_IPS=("${SERVER_IPS[@]}")
+CONTAINER_SERVER_IPS=("${SERVER_IPS[@]}")
 
-echo "raw list:" ${SERVER_IPS[@]}
+if [[ -z "${threadCountForLast}" || "${threadCountForLast}" -eq "0" ]]; then
+  echo "NO need thread count for last"
+else  
+  SERVER_IPS+=("${LGSG_IP}")
+fi
+
+echo "slave list:" ${SERVER_IPS[@]}
 echo "threadCountBase: " $threadCountBase
 echo "threadCountForLast: " $threadCountForLast
 
-if [[ -z "${threadCountForLast}" || "${threadCountForLast}" -eq "0" ]]; then
-	echo "NO need thread count for last"
-else
-	LAST_SERVER=${SERVER_IPS[-1]}	
-	unset "SERVER_IPS[-1]"
-fi
-
 #2
 timestamp=$(date +%Y%m%d_%H%M%S)
-jmeter_path=/mnt/jmeter
+data_src=/mnt/jmeter
 TEST_NET=perfsamplenet
 
 #3
@@ -43,54 +44,38 @@ echo "Create testing network"
 docker network create --subnet=$SUB_NET $TEST_NET
 
 #4
-echo "Create JMeter slave"
-for IP_ADD in "${SERVER_IPS[@]}"
+echo "Create JMeter container slave"
+for IP_ADD in "${CONTAINER_SERVER_IPS[@]}"
 do
-echo adding $IP_ADD
+echo creating $IP_ADD
 docker run \
 -dit \
 --net $TEST_NET --ip $IP_ADD \
--v "${data_folder}":${jmeter_path} \
+-v "${data_folder}":${data_src} \
 --rm \
-jmeter \
+jmeter-base \
 -n -s \
--Jserver.rmi.ssl.keystore.file=${jmeter_path}/keys/rmi_keystore.jks \
+-Jserver.rmi.ssl.keystore.file=${data_src}/keys/rmi_keystore.jks \
 -Jclient.rmi.localport=7000 -Jserver.rmi.localport=60000 \
--JnumberOfThreads=${threadCountBase} -JappName=${targetVersion} -JloopCount=1 \
--j ${jmeter_path}/server/slave_${timestamp}_${IP_ADD:9:3}.log 
+-j ${data_src}/server/slave_${timestamp}_${IP_ADD:9:3}.log 
 done
 
-if [ ! -z "${LAST_SERVER}" ]; then
-	echo "Create LAST JMeter slave"
-	docker run \
-	-dit \
-	--net $TEST_NET --ip ${LAST_SERVER} \
-	-v "${data_folder}":${jmeter_path} \
-	--rm \
-	jmeter \
-	-n -s \
-	-Jserver.rmi.ssl.keystore.file=${jmeter_path}/keys/rmi_keystore.jks \
-	-Jclient.rmi.localport=7000 -Jserver.rmi.localport=60000 \
-	-JnumberOfThreads=${threadCountForLast} -JappName=${targetVersion} -JloopCount=1 \
-	-j ${jmeter_path}/server/slave_${timestamp}_${LAST_SERVER:9:3}.log 
-fi
-
 #5 
-echo "Create JMeter master"
+echo "Create JMeter container master"
 docker run \
   --net $TEST_NET --ip $CLIENT_IP \
-  -v "${data_folder}":${jmeter_path} \
+  -v "${data_folder}":${data_src} \
   --rm \
-  jmeter \
+  jmeter-master \
   -n -X \
-  -Jserver.rmi.ssl.keystore.file=${jmeter_path}/keys/rmi_keystore.jks \
+  -Jserver.rmi.ssl.keystore.file=${data_src}/keys/rmi_keystore.jks \
   -Jclient.rmi.localport=7000 \
-  -Jremote_hosts $(echo $(printf ",%s" "${ORIGINAL_SERVER_IPS[@]}") | cut -c 2-) \
-  -JnumberOfThreads=${threadCountBase} -JappName=${targetVersion} -JloopCount=1 \
-  -t ${jmeter_path}/jmx/Weather.jmx \
-  -l ${jmeter_path}/client/result_${timestamp}.jtl \
-  -j ${jmeter_path}/client/jmeter_${timestamp}.log 
+  -Jremote_hosts $(echo $(printf ",%s" "${SERVER_IPS[@]}") | cut -c 2-) \
+  -GnumberOfThreads=${threadCountBase} -DappName=${targetVersion} -GloopCount=1 \
+  -t ${data_src}/jmx/Weather.jmx \
+  -l ${data_src}/client/result_${timestamp}.jtl \
+  -j ${data_src}/client/jmeter_${timestamp}.log 
 
 #6
-docker ps -a | awk '{ print $1,$2 }' | grep jmeter | awk '{print $1 }' | xargs -I {} docker rm {} -f
+docker ps -a | awk '{ print $1,$2 }' | grep ^jmeter | awk '{print $1 }' | xargs -I {} docker rm {} -f
 docker network rm $TEST_NET
